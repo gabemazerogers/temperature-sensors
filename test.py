341fa429464b784cf02079f513ce980a48dbeb77
Niral Pathak <niralpathak@gmail.com>
2016-02-29 19:46:17 -0800

    working read/write at same time; not temper-poll

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
 import os,sched,time
+
 s = sched.scheduler(time.time, time.sleep)
+f = open('out.txt','w',0)
+
+#successful read/write simultaneoulsy if including 0 param above
+
+#march-6: write to ieng6 and read; run temper-poll command with pi
+
 def do_something(sc):
 	i = 0
-	f = open('out.txt','w')
 	while i < 10 :
 		f.write(str(i)) 
 		i = i + 1
 	sc.enter(5,1,do_something, (sc,))
 s.enter(5, 1, do_something, (s,))
 s.run()
-
-
-# timeout = 5.0
-
-# def doWork():
-# 	print "hi"
-# 	pass
-# l = task.LoopingCall(doWork)
-# l.start(timeout)
-# reactor.run()
