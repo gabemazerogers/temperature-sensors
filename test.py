390da2498ba45816bc754d219e017a48129682e9
Gabe Maze-Rogers <gmazerogers@gmail.com>
2016-02-29 19:31:36 -0800

    updated scheduled write file

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
-# import os,sched,time
-# s = sched.scheduler(time.time, time.sleep)
-# def do_something(sc):
-# 	os.system('ls -l >> out.txt')
-# 	print (type(sc))
-# 	sc.enter(5,1,do_something, (sc,))
-# s.enter(5, 1, do_something, (s,))
-# s.run()
+import os,sched,time
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc):
+	i = 0
+	f = open('out.txt','w')
+	while i < 10 :
+		f.write(str(i)) 
+		i = i + 1
+	sc.enter(5,1,do_something, (sc,))
+s.enter(5, 1, do_something, (s,))
+s.run()
 
-import twisted
 
-timeout = 5.0
+# timeout = 5.0
 
-def doWork():
-	print "hi"
-	pass
-l = task.LoopingCall(doWork)
-l.start(timeout)
-reactor.run()
+# def doWork():
+# 	print "hi"
+# 	pass
+# l = task.LoopingCall(doWork)
+# l.start(timeout)
+# reactor.run()
