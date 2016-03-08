bf7a3c90e32c22201f0eae6f5c7b6b12e0c6a55f
Gabe Maze-Rogers <gmazerogers@gmail.com>
2016-03-07 19:34:47 -0800

    Added example SFTP connection program

--- sftp.py
+++ sftp.py
@@ -1,7 +1,14 @@
-import pysftp
+import pysftp, re
 serverName = 'ieng6.ucsd.edu'
 username = "" # REPLACE WITH YOURS
 password = "" # REPLACE WITH YOURS
-filePath = 'python/temp.txt'
+filePath = ''
 with pysftp.Connection(serverName,username=username,password=password, port=22) as sftp:
 	sftp.get(filePath, '')
+
+tempFile = open(filePath, 'r')
+lines = tempFile.readlines()
+lines = ''.join(lines)
+print lines
+temp_f = re.findall('\d+\.?\d*', lines)
+print temp_f[3]
