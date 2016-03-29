f40dc84beebe587b294541e75fb6d696afc0a124
Niral Pathak <niralpathak@gmail.com>
2016-03-28 18:37:05 -0700

    moved important code to Source/

--- Source/format.py
+++ Source/format.py
@@ -2,17 +2,14 @@ import pysftp, re
 
 """sftp.py: Opens a connection with ieng6 (or whatever server) and reads the output of the temper poll command and locates the farenheit temperature using regex"""
 
-serverName = 'ieng6.ucsd.edu'
-username = "" # REPLACE WITH YOURS
+serverName = 'acsweb.ucsd.edu'
+username = "ewb" # REPLACE WITH YOURS
 password = "" # REPLACE WITH YOURS
-filePath = ''
+
+filePath = '~/Temperature_Project_2016/rawData.txt'
+# rawData.txt holds unformatted temper-poll results, the date, and newline char
+
 with pysftp.Connection(serverName,username=username,password=password, port=22) as sftp:
 	sftp.get(filePath, '')
 
-tempFile = open(filePath, 'r')
-
-lines = tempFile.readlines() # initialized as list
-temp_f = re.findall('\d+\.?\d*', lines[1])
-print temp_f[2]
-fullDate = dateutil.parser.parse(lines[2])
-print fullDate
+# TODO
---
+++ Source/updateLogFile.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# keep the above encoding message; DO NOT DELETE
+
+# This file calls temper-poll and date; then, we put these unformatted strings
+# to rawData.txt where we then parse appropriate values and create/append a .csv
+
+
+import os, re, dateutil.parser
+
+location = "ERC"
+
+# delete below line once we are ready to run on Rpi
+os.system('echo $"Found 1 devices\nDevice #0: 22.5°C 72.5°F" > rawData.txt')
+
+# the first call to temper-poll does not append because we want to wipe all
+# previous data
+#os.system('temper-poll > rawData.txt')
+os.system('date >> rawData.txt')
+
+rawData = open('rawData.txt', 'r')
+
+lines = rawData.readlines() # initialized as list
+temp_f = re.findall('\d+\.?\d*', lines[1])
+# temp_f[2] = fahrenheit value
+
+fullDate = dateutil.parser.parse(lines[2])
+# fullDate = formatted date
+
+logFile = open('formattedData.csv', 'w')
+logFile.write(str(fullDate) + ", " + str(temp_f[2]) + ", " + location)
