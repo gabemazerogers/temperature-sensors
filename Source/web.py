b7d897f3128e7a3345bf143bdc67e9aa1dac9489
Gabe Maze-Rogers <gmazerogers@gmail.com>
2016-04-06 17:19:14 -0700

    Moved working flask file to source

--- Source/web.py
+++ Source/web.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 import csv
 
-with open('readings.csv') as csv_file:
+with open('formattedData.csv') as csv_file:
 	data = csv_file.readlines()
 csv_data = csv.DictReader(data)
 print data
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_CSV():
-	with open('readings.csv') as csv_file:
+	with open('formattedData.csv') as csv_file:
 		data = csv_file.readlines()
 	csv_data = csv.DictReader(data)
 	return render_template("index.html", title="Temperature Data", data=csv_data)
